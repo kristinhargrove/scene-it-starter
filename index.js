7896ca6c33beea767ec6318b0b4e00a254b57f1c
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-10-15 20:09:10 -0400

    implemented saveToWatchlist(hopefully)

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+function saveToWatchlist(imdbID) { //saves users movies to their watch list
+
+	var movie = movieData.find(function(currentMovie) {
+		return currentMovie.imdbID == imdbID;
+		})
+
+		var watchlistJSON = localStorage.getItem('watchlist');
+		var watchlist = JSON.parse(watchlistJSON);
+
+		if (watchlist == null) {
+			watchlist = [];
+		}
+
+		watchlist.push(movie);
+		watchlistJSON = JSON.stringify(watchlist);
+		localStorage.setItem('watchlist', watchlistJSON);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	let renderMovies = (movieArray) => {
 
-		function saveToWatchlist(imdbID) {
-			return movieData.imdbID;
-		}
-
 		let finalHTMLArray = movieArray.map(function(currentMovie) {
 			return `
-				<div class="movie card">
-					<img class="card-img-top" src="${currentMovie.Poster}" alt="${currentMovie.Title}" /></img>
+				<div class="movie card" style="width: 18rem; height: 35rem;">
+					<img class="card-img-top" src="${currentMovie.Poster}" alt="${currentMovie.Title}"/></img>
 					<div class="card-img-bottom">
 						<h5 class="card-title movie-title">${currentMovie.Title}</h5>
 						<span class="year">${currentMovie.Year}</span>
-						<div><button onclick="saveToWatchlist(${currentMovie.imdbID})type="button" class="btn btn-primary render-button mt-1">Add</button></div>
+						<div><button onclick="saveToWatchlist('${currentMovie.imdbID}')" type="button" class="btn btn-primary render-button mt-1">Add</button></div>
 					</div>
 					
 				</div>	
